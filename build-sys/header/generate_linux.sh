eb0ddc7c18d0db63aa4880e24c089bc7a0904fdb
Ben Wiebe <ben.wiebe@windriver.com>
2017-07-14 11:07:42 -0400

    build: fix os.h generation scripts
    This commit fixes an issue with the Windows os.h generation script not copying
    the input files as expected. In addition, no message will be displayed by rm
    if os.h doesn't exist.

--- build-sys/header/generate_linux.sh
+++ build-sys/header/generate_linux.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-rm os.h
+echo "Removing old os.h (if present)"
+rm -f os.h
 
 osname=$(lsb_release -si) # get os name
 osname=$(echo $osname | awk '{print tolower($0)}') # to lower case
 
 if [[ $osname == "android" ]]; then
-	echo "*** Generating os.h for Android ***"
+	echo "Generating os.h for Android"
 	cat build-sys/header/os_h_top.in os/os_android.h os/os_posix.h os/os_posix_macros.h build-sys/header/os_h_bot.in > os.h
 
 elif [[ $osname == "vxworks" ]]; then
-	echo "*** Generating os.h for VxWorks ***"
+	echo "Generating os.h for VxWorks"
 	cat build-sys/header/os_h_top.in os/os_vxworks.h os/os_posix.h os/os_posix_macros.h build-sys/header/os_h_bot.in > os.h
 
 else
-	echo "*** Generating os.h for generic Linux ***"
+	echo "Generating os.h for generic Linux"
 	cat build-sys/header/os_h_top.in os/os_posix.h os/os_posix_macros.h build-sys/header/os_h_bot.in > os.h
 
 fi
+
+echo "os.h setup done!"
--- build-sys/header/generate_windows.bat
+++ build-sys/header/generate_windows.bat
@@ -1,4 +1,6 @@
 @echo off
 
-del os.h
-copy build-sys/header/os_h_top.in + os/os_win.h + os/os_win_macros.h + build-sys/header/os_h_bot.in os.h
+IF EXIST "os.h" (
+	del "os.h"
+)
+copy "build-sys\header\os_h_top.in" + "os\os_win.h" + "os\os_win_macros.h" + "build-sys\header\os_h_bot.in" "os.h"
