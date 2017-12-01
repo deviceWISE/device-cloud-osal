d41e5c6e5bbac9950ffafa1386af39bc700e9125
Paul Barrette <paulbarrette@gmail.com>
2017-12-01 11:03:33 -0500

    copyright: update copyright for Apache 2.0
    Update all copyright to Apache 2.0.
    
    Signed-off-by: Paul Barrette <paulbarrette@gmail.com>

--- build-sys/cmake/TestSupport.cmake
+++ build-sys/cmake/TestSupport.cmake
@@ -1,10 +1,15 @@
 #
 # Copyright (C) 2015-2017 Wind River Systems, Inc. All Rights Reserved.
 #
-# The right to copy, distribute or otherwise make use of this software may
-# be licensed only pursuant to the terms of an applicable Wind River license
-# agreement.  No license to Wind River intellectual property rights is granted
-# herein.  All rights not licensed by Wind River are reserved by Wind River.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software  distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, either express or implied.
 #
 
 include( CMakeParseArguments )
