be334d0dc66577c94fc4ca0b53863eb46adcaf6f
Paul Barrette <paulbarrette@gmail.com>
2017-12-05 17:40:01 -0500

    copyright: add copyright to some header files.
    Add copyright.
    
    Signed-off-by: Paul Barrette <paulbarrette@gmail.com>

--- src/os_win.h
+++ src/os_win.h
@@ -1,3 +1,20 @@
+/**
+ * @file
+ * @brief Function definitions for WINDOWS operating systems
+ *
+ * @copyright Copyright (C) 2016-2017 Wind River Systems, Inc. All Rights Reserved.
+ *
+ * @license Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied."
+ */
+
 /** @brief Don't support advanced Windows kernel calls */
 #define WIN32_LEAN_AND_MEAN
 #pragma warning( push, 1 )
