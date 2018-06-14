6af7afabf438b99116da029f08d6245498f3614a
Keith Holman <keith.holman@windriver.com>
2018-06-14 16:23:30 -0400

    build: remove duplicate build functions
    This patch removes the version functions from the "BuildSupport.cmake"
    list because previously newer versions where defined in
    "VersionSupport.cmake".
    
    Signed-off-by: Keith Holman <keith.holman@windriver.com>

--- build-sys/cmake/BuildSupport.cmake
+++ build-sys/cmake/BuildSupport.cmake
@@ -58,58 +58,6 @@ endfunction( GET_SHORT_NAME )
 get_short_name( PACKAGE_NAME_SHORT "${PACKAGE_NAME}" )
 get_short_name( PROJECT_NAME_SHORT "${PROJECT_NAME}" )
 
-### Parse version information
-# Support function for extracting a version number from a string, if the
-# index is not specified it will return a value of "0" for that version part
-# Arguments:
-# - _out           output variable name
-# - _list_name     list containing version seperated
-# - _index         index to extract
-function( VERSION_PART_FROM_LIST _out _list_name _index )
-	set( VERSION "0" )
-	list( LENGTH ${_list_name} VERSION_ITEMS )
-	if( ${VERSION_ITEMS} GREATER ${_index} )
-		list( GET ${_list_name} ${_index} VERSION )
-		# strip whitespace
-		string( STRIP "${VERSION}" VERSION )
-		# strip non-numeric characters
-		string( REGEX REPLACE "[^0-9]+" "" VERSION "${VERSION}" )
-		# strip leading zeros
-		string( REGEX REPLACE "^[0]+([1-9])" "\\1" VERSION "${VERSION}" )
-	endif( ${VERSION_ITEMS} GREATER ${_index} )
-	set( "${_out}" ${VERSION} PARENT_SCOPE )
-endfunction( VERSION_PART_FROM_LIST )
-
-# Extracts the various version parts (major, minor, patch, tweak) from a
-# version string The values will be saved as:
-# - ${_out}_COUNT: number of version parts set
-# - ${_out}_MAJOR: major (1st) version number
-# - ${_out}_MINOR: minor (2nd) version number
-# - ${_out}_PATCH: patch (3rd) version number
-# - ${_out}_TWEAK: tweak (4th) version number
-#
-# Arugments:
-# - _out           output variable name
-# - _ver ...       version to split
-function( VERSION_SPLIT _out _ver )
-	set( _ver ${_ver} ${ARGN} )
-	string( REPLACE "." ";" VERSION_SPLIT "${_ver}" )
-
-	list( LENGTH VERSION_SPLIT "VERSION_COUNT" )
-
-	version_part_from_list( VERSION_MAJOR VERSION_SPLIT 0 )
-	version_part_from_list( VERSION_MINOR VERSION_SPLIT 1 )
-	version_part_from_list( VERSION_PATCH VERSION_SPLIT 2 )
-	version_part_from_list( VERSION_TWEAK VERSION_SPLIT 3 )
-
-	set( "${_out}_COUNT" "${VERSION_COUNT}" PARENT_SCOPE )
-	set( "${_out}_MAJOR" "${VERSION_MAJOR}" PARENT_SCOPE )
-	set( "${_out}_MINOR" "${VERSION_MINOR}" PARENT_SCOPE )
-	set( "${_out}_PATCH" "${VERSION_PATCH}" PARENT_SCOPE )
-	set( "${_out}_TWEAK" "${VERSION_TWEAK}" PARENT_SCOPE )
-endfunction( VERSION_SPLIT )
-version_split( PROJECT_VERSION "${PROJECT_VERSION}" )
-
 ### Generate Copyright String ###
 if( WIN32 )
 	execute_process( COMMAND "cmd" "/C" "date" "/T" OUTPUT_VARIABLE CURRENT_YEAR )
